d97682b0bf60c5ad87c3122ad8ddb40b75bb32e1
MariaIoana89 <maria_ioana28@yahoo.com>
2018-06-26 20:22:31 +0300

    the player and enemis appear on screen

--- js/app.js
+++ js/app.js
@@ -14,48 +14,50 @@
             // a helper we've provided to easily load images
             this.sprite = 'images/enemy-bug.png';
         }
+
+        // Update the enemy's position, required method for game
+        // Parameter: dt, a time delta between ticks
+        update(dt) {
+            // You should multiply any movement by the dt parameter
+            // which will ensure the game runs at the same speed for
+            // all computers.
+            this.x += this.speed * dt;
+            if (this.x > 500) {
+                this.x = -100;
+            }
+        };
+
+        //Reset enemy's position and change their speed
+        /*if (this.x > canvasLimits.width) {
+            this.x = -100;
+            this.speed = 100 + Math.floor(Math.random() * 400);
+        }
+*/
+        // Draw the enemy on the screen, required method for game
+        render() {
+            ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+        };
     }
-    // Update the enemy's position, required method for game
-    // Parameter: dt, a time delta between ticks
-    Enemy.prototype.update = function(dt) {
-        // You should multiply any movement by the dt parameter
-        // which will ensure the game runs at the same speed for
-        // all computers.
-        this.x += this.speed * dt;
-    };
-
-    //Reset enemy's position and change their speed
-    if (this.x > canvasLimits.width) {
-        this.x = -100;
-        this.speed = 100 + Math.floor(Math.random() * 400);
-    }
-
-    // Draw the enemy on the screen, required method for game
-    Enemy.prototype.render = function() {
-        ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-    };
-
     // Now write your own player class
     // This class requires an update(), render() and
     // a handleInput() method.
     class Player {
-        constructor(x, y, speed) {
+        constructor(x, y) {
             this.x = x;
             this.y = y;
-            this.speed = speed;
             this.sprite = 'images/char-princess-girl.png';
         }
-    }
-    //update method
-    Player.prototype.update = function(dt) {
 
-        }
-        //render method
-    Player.prototype.render = function() {
-            ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-        }
-        //handleInput method
-    Player.prototype.handleInput = function(keypress) {
+        //update method
+        update() {
+
+            }
+            //render method
+        render() {
+                ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+            }
+            //handleInput method
+        handleInput() {
             switch (keypress) {
                 case 'left':
                     this.x -= this.speed + 50;
@@ -70,11 +72,17 @@
                     break;
             }
         }
-        // Now instantiate your objects.
-        // Place all enemy objects in an array called allEnemies
-        // Place the player object in a variable called player
-    var allEnemies = [];
-    var player = new Player();
+    }
+    // Now instantiate your objects.
+    // Place all enemy objects in an array called allEnemies
+    // Place the player object in a variable called player
+    const allEnemies = [];
+    const enemy1 = new Enemy(30, 50, 300);
+    const enemy2 = new Enemy(30, 150, 200);
+    const enemy3 = new Enemy(30, 250, 100);
+    allEnemies.push(enemy1, enemy2, enemy3);
+    const player = new Player(200, 400);
+
     //win the game
     if (player.y < 1) {
         showModal();
